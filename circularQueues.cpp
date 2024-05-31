5d5515862f506770532634e711d527fc08375a8b
riqsans <thrq88@gmail.com>
2024-05-31 14:32:28 +0700

    menuliskan akses modifier public untuk class queues

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -13,6 +13,13 @@ public:
     void insert(){
         int num;
         cout << "Enter a Number:";
-        
+        cin >> num;
+        cout << endl;
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
     }
 }
