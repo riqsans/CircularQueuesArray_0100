3bf029f79fba57e7d1400e972c0a8090e812ab7f
riqsans <thrq88@gmail.com>
2024-05-31 15:09:37 +0700

    menuliskan void display

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -83,8 +83,20 @@ public:
                 cout << queue_array[FRONT_position] << " " ;
                 FRONT_position++;
             }
+            
+            FRONT_position = 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
             cout << endl;
         }
-
     }
 };
+
+int main(){
+    Queues q;
+    char ch;
+}
