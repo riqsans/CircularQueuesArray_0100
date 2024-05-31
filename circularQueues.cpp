c26a93ca37c5e0e121c146fd07902150b62855bb
riqsans <thrq88@gmail.com>
2024-05-31 14:29:13 +0700

    menuliskan cout enter a number

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 class Queues {
-    
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+    void insert(){
+        int num;
+        cout << "Enter a Number:";
+        
+    }
 }
