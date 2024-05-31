d90cd02eb721e29e8456fa8b7f7b9b4eb3f7d308
riqsans <thrq88@gmail.com>
2024-05-31 14:46:11 +0700

    menuliskan fungsi if

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -43,5 +43,10 @@ public:
         }
         cout << "n\The element deleted from the queue is: " << queue_array[FRONT] << "\n";
         
+        // cek jika antrian hanya memiliki satu elemen
+        if(FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 }
