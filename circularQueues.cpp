a50cad349d944902ccd00863a8f36f207b846c93
riqsans <thrq88@gmail.com>
2024-05-31 14:55:53 +0700

    menuliskan fungsi if dan while untuk void display

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -70,6 +70,12 @@ public:
         cout << "\nElements in the queue are ...\n";
 
         //Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
-        
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
+
     }
 }
